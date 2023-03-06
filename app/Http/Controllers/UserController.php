cd94b31fd28a78e3792d22d6ad87dda446148efe
mateo-orozco <oromateo@hotmail.com>
2023-03-06 18:06:47 -0500

    mostrar usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UserController extends Controller
 {
     //
+    public function index(){
+        $users =User::all();
+        return $users;
+    }
+
+    public function show($id){
+        $user = user::find($id);
+        return $user;
+    }
+    
 }
