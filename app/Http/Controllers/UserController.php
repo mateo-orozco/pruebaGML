8d826d596676337462722e0e1ffa73f46aa14459
mateo-orozco <oromateo@hotmail.com>
2023-03-07 00:08:48 -0500

    creacion de CRUD y rutas en back

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,5 +16,49 @@ class UserController extends Controller
         $user = user::find($id);
         return $user;
     }
-    
+
+    public function create(Request $request){
+        try{
+            $request->validate([
+                'name' => 'string|required|max:100|min:5|alpha:ascii',
+                'lastname' => 'required|string|max:100|alpha:ascii',
+                'CC' => 'required|numeric',
+                'country' => 'required|string',
+                'email' => 'required|unique:User,email|email:rfc,dns|max:150',
+                'address' => 'required|string|max:180',
+                'phone' => 'required|numeric|digits:10',
+                'category_id' => 'required'
+            ]);
+        }catch(\throwable $th){
+            return response()->json(['error' => $th->getMessage()], 400);
+        }
+        $user = User::create($request->all());
+        return "User created successfully";
+    }
+
+    public function update(Request $request){
+        try{
+            $request->validate([
+                'name' => 'string|required|max:100|min:5|alpha:ascii',
+                'lastname' => 'required|string|max:100|alpha:ascii',
+                'CC' => 'required|numeric',
+                'country' => 'required|string',
+                'email' => 'required|unique:User,email|email:rfc,dns|max:150',
+                'address' => 'required|string|max:180',
+                'phone' => 'required|numeric|digits:10',
+                'category_id' => 'required'
+            ]);
+        }catch(\throwable $th){
+            return response()->json(['error' => $th->getMessage()], 400);
+        }
+        $user = User::find($request->id);
+        $user->update($request->all());
+        return "User updated successfully";
+    }
+
+    public function destroy($id){
+        $user = User::find($id);
+        $user->delete();
+        return "User deleted successfully";
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix'=>'user'], function($router){
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('/create', [UserController::class, 'create']);
+    Route::put('/update/{id}', [UserController::class, 'update']);
+    Route::delete('/destroy/{id}', [UserController::class, 'destroy']);
 });
